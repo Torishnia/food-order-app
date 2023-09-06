19fea2974e435b851ea7677099d8eb020b87787b
Valeri <valeri.torishnya@gmail.com>
2023-09-06 15:46:46 +0300

    add cartContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
+
 import Cart from "./components/Cart";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -15,13 +17,13 @@ function App() {
   }
 
   return (
-    <>
+    <CartProvider>
       {cartIsShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
