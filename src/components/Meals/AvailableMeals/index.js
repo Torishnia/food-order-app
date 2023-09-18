513e276f4f7b89bf550f9b70af42df993c05845b
Valeri <valeri.torishnya@gmail.com>
2023-09-18 13:27:47 +0300

    get data from database

--- src/components/Meals/AvailableMeals/index.js
+++ src/components/Meals/AvailableMeals/index.js
@@ -1,26 +1,59 @@
-import { DUMMY_MEALS } from '../../../utils/data';
+import { useEffect, useState } from 'react';
+
 import Card from '../../UI/Card';
 import MealItem from '../MealItem';
-
 import styles from './index.module.scss';
 
 const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch('https://food-order-fcd1a-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    }
+
+    fetchMeals();
+  }, [])
+
   return (
-    <section className={styles.meals}>
-      <Card>
-        <ul>
-          {DUMMY_MEALS.map((meal) => 
-            <MealItem
-              key={meal.id}
-              id={meal.id}
-              name={meal.name}
-              description={meal.description}
-              price={meal.price}
-            />
-          )}
-        </ul>
-      </Card>
-    </section>
+    <>
+      {isLoading
+        ? (<section className={styles.meals_loading}>
+            <p>Loading...</p>
+          </section>)
+        : (<section className={styles.meals}>
+            <Card>
+              <ul>
+                {meals.map((meal) => 
+                  <MealItem
+                    key={meal.id}
+                    id={meal.id}
+                    name={meal.name}
+                    description={meal.description}
+                    price={meal.price}
+                  />
+                )}
+              </ul>
+            </Card>
+          </section>)
+      }
+    </>
   )
 }
 
